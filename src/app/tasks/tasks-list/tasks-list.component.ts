0166ed1c67878921325b803e3620c4ea0331c627
Michael Doh <michaeldoh04@gmail.com>
2025-05-18 23:53:33 -0500

    Angular Services tutorial

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskService } from '../tasks.service';
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -8,10 +10,30 @@ import { TaskItemComponent } from './task-item/task-item.component';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionsProvider],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private TaskService = inject(TaskService);
+  private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.TaskService.allTasks().filter(
+          (task) => task.status === 'OPEN'
+        );
+      case 'in-progress':
+        return this.TaskService.allTasks().filter(
+          (task) => task.status === 'IN_PROGRESS'
+        );
+      case 'done':
+        return this.TaskService.allTasks().filter(
+          (task) => task.status === 'DONE'
+        );
+      default:
+        return this.TaskService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
